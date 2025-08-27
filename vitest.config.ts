58a5086c9b4a08ae2d5a6637f2280019357d8368
Otto <ottoclarke@icloud.com>
2025-08-27 13:56:10 +0100

    chore: add testing and lint setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plugins: [react()],
   test: {
-    environment: "node",
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    globals: true,
   },
 });
